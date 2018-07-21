e97a7481cf5ac205b81acb53532bc6e6cd63a10a
diamond95 <ivanvk95@gmail.com>
2018-07-21 18:35:27 +0200

    Fixed bugs

--- chat/index.php
+++ chat/index.php
@@ -1,7 +1,6 @@
 <?php
 include('database_connection.php');
 session_start();
-
 if(!isset($_SESSION['user_id'])){
 	header("location:login.php");
 }
@@ -63,17 +62,13 @@ if(!isset($_SESSION['user_id'])){
 	</div>
 	</body>
 </html>  
-
 <script>  
 $(document).ready(function(){
-
  fetch_user();
-
  setInterval(function(){
 	update_last_activity();
 	fetch_user();
  }, 5000);
-
  function fetch_user() {
 	$.ajax({
 		url:"fetch_user.php",
@@ -83,14 +78,12 @@ $(document).ready(function(){
 		}
 	})
  }
-
  function update_last_activity() {
 	$.ajax({
 		url:"update_last_activity.php",
 		success:function(){}
 	})
  }
-
 function make_chat_dialog_box(to_user_id, to_user_name) {
 	var modal_content = '<div id="user_dialog_'+to_user_id+'" class="user_dialog" style="background-color:" title="You have chat with '+to_user_name+'">';
 	modal_content += '<div style="height:400px; border:1px solid #ccc; overflow-y: scroll; margin-bottom:24px; padding:16px;" class="chat_history" data-touserid="'+to_user_id+'" id="chat_history_'+to_user_id+'">';
@@ -101,7 +94,6 @@ function make_chat_dialog_box(to_user_id, to_user_name) {
 	modal_content+= '<button type="button" name="send_chat" id="'+to_user_id+'" class="btn btn-info send_chat">Send</button></div></div>';
 	$('#user_model_details').html(modal_content);
 }
-
 $(document).on('click', '.start_chat', function(){
 	var to_user_id = $(this).data('touserid');
 	var to_user_name = $(this).data('tousername');
@@ -130,7 +122,6 @@ function refreshMsg() {
 $( ".selector" ).on( "dialogclose", function( event, ui ) {
 		//alert("todo - later");
 });
-
 $(document).on('click', '.send_chat', function(){
 	$.playSound("mp3/click.mp3") // here we set MP3 SOUND FOR "send" button click
 	var to_user_id = $(this).attr('id');
